505cce4ad697f4ec24a7a2a52f150362b2a3906b
Richard Beese <richardbeese@Richards-MacBook-Air.local>
2017-03-12 17:12:33 -0600

    Finished app with bugs

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import React from 'react';
+import { Router } from 'react-router';
+// import createBrowserHistory from 'history/createBrowserHistory';
+import { Provider } from 'react-redux';
+import configureStore from './store/configStore';
 import routes from './routes';
+
+// export const history = createBrowserHistory();
+const store = configureStore();
 
 // We require the routes and render to the DOM using ReactDOM API
 ReactDOM.render(
-    <Router history={browserHistory} routes={routes} />,
-    document.getElementById('root')
+    // <h1>Hello World</h1>,
+    <Provider store={store}>
+       <Router routes={routes} />
+     </Provider>, document.getElementById('root')
 );
